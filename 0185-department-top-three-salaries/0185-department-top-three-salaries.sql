14e93657925ab212a531007cef63a3a26b547b89
Aaryan verma <aaryanverma751@gmail.com>
2026-05-30 00:11:22 +0530

    Time: 1413 ms (10.71%), Space: 0B (100%) - LeetHub

---
+++ 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
@@ -0,0 +1,12 @@
+# Write your MySQL query statement below
+# Write your MySQL query statement below
+select d.name as Department , e.name as Employee , e.Salary as Salary
+from Employee e
+left join Department d
+on e.departmentId=d.id
+where (
+    SELECT COUNT(DISTINCT e2.salary)
+    FROM Employee e2
+    WHERE e2.departmentId = e.departmentId
+    AND e2.salary > e.salary
+) < 3;
