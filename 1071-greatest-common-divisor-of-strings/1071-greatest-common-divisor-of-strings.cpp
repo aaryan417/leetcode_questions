65056aebfcc543e8a1306752c5f73493e1a4208f
Aaryan verma <aaryanverma751@gmail.com>
2026-06-18 15:15:38 +0530

    Time: 0 ms (100%), Space: 9.7 MB (60.87%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
@@ -1,9 +1,19 @@
 class Solution {
 public:
     string gcdOfStrings(string str1, string str2) {
-     if (str1 + str2 == str2 +str1){
-        return str1.substr(0,gcd(str1.size(),str2.size())); 
-     } 
-     return "";  
+
+        // ------>>>>>>>>>>>>approch 1
+    //  if (str1 + str2 == str2 +str1){
+    //     return str1.substr(0,gcd(str1.size(),str2.size())); 
+    //  } 
+    //  return "";  
+
+
+
+/// aproch 2----------->>>>>>>>
+
+
+ return (str1 + str2 == str2 +str1)?
+     str1.substr(0,gcd(str1.size(),str2.size())):"";   
     }
 };
