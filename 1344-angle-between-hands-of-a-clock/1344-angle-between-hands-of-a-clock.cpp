7a68bcec989e31a889b5de54953008f1f5f2e7c2
Aaryan verma <aaryanverma751@gmail.com>
2026-06-18 15:04:21 +0530

    Time: 0 ms (100%), Space: 8 MB (74.16%) - LeetHub

--- 1344-angle-between-hands-of-a-clock/1344-angle-between-hands-of-a-clock.cpp
+++ 1344-angle-between-hands-of-a-clock/1344-angle-between-hands-of-a-clock.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     double angleClock(int hour, int minutes) {
        double minutes_angle=6* minutes;
-       double hour_angle =30* hour + minutes/60.0*30;
+       double hour_angle =30* hour + 0.5 * minutes;
        double deff_angle =abs(hour_angle - minutes_angle);
        return  min(deff_angle, 360-deff_angle);
 
