f28933b94f3e5988e72626594c5eff99e045cfc2
Aaryan verma <aaryanverma751@gmail.com>
2026-08-05 22:57:37 +0530

    Time: 1223 ms (6.96%), Space: 0B (100%) - LeetHub

--- 1587-bank-account-summary-ii/1587-bank-account-summary-ii.sql
+++ 1587-bank-account-summary-ii/1587-bank-account-summary-ii.sql
@@ -2,7 +2,7 @@ SELECT
     u.name,
     SUM(t.amount) AS balance
 FROM Users u
-JOIN Transactions t
+right JOIN Transactions t
 ON u.account = t.account
 GROUP BY u.account, u.name
 HAVING SUM(t.amount) > 10000;
